d0ab882d2bc8a2d800418514e0bd36ef24564cce
MAttila42 <65223119+MAttila42@users.noreply.github.com>
2025-11-04 15:43:33 +0100

    ♻️ refactor: removed cloudflare hack, since we might not need it

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,7 @@
-import type { Env } from 'bun'
-import type { Context } from 'elysia'
 import { cors } from '@elysiajs/cors'
 import { Elysia } from 'elysia'
 
-const app = new Elysia({ strictPath: false, aot: false })
+new Elysia({ strictPath: false, aot: false })
   .use(cors())
   .get('/', 'Very simple selfhostable proxy service for REST requests. More info: https://github.com/MAttila42/rest-proxy')
-
-export default {
-  async fetch(
-    request: Request,
-    _env: Env,
-    _ctx: Context,
-  ): Promise<Response> {
-    return await app.fetch(request)
-  },
-}
+  .listen(3000)
